6d8487596bbf82a18038c4d71622747e1cd28884
Matt Lewis <matt@mplewis.com>
2023-08-24 23:51:21 -0700

    feat: working init

--- root/templates/rclone.sh
+++ root/templates/rclone.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euxo pipefail
 
-rclone mount \
-	--config /config/rclone.conf \
-	--allow-other \
-	--buffer-size=0 \
-	--cutoff-mode=cautious \
-	--dir-cache-time 10s \
-	--multi-thread-streams=0 \
-	--network-mode \
-	--read-only \
-	--vfs-cache-mode minimal \
-	provider:/links /media
+args=(
+	# location of the config file
+	--config /config/rclone.conf
+
+	# connect to the source named [provider] in rclone.conf
+	# and mount the remote /links directory into local /media
+	mount
+	provider:/links
+	/media
+
+	# allow user `plex` to access our files
+	--allow-other
+
+	# recommended settings for AllDebrid:
+	# https://help.alldebrid.com/en/faq/rclone-webdav
+	--buffer-size=0
+	--cutoff-mode=cautious
+	--dir-cache-time 10s
+	--multi-thread-streams=0
+	--network-mode
+	--read-only
+	--vfs-cache-mode minimal
+)
+
+rclone "${args[@]}"
