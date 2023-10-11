0532f978593b91e88aa235558a8d20ae0b71422b
Matt Lewis <matt@mplewis.com>
2023-10-10 21:19:50 -0600

    feat: version in sync with tag

---
+++ connector/src/util/version.spec.ts
@@ -0,0 +1,15 @@
+import execa from "execa";
+import { describe, expect, it } from "vitest";
+
+import { VERSION } from "./version";
+
+describe("version", () => {
+  it("matches the latest tag", () => {
+    const latestTag = execa.sync("git", [
+      "describe",
+      "--tags",
+      "--abbrev=0",
+    ]).stdout;
+    expect(`v${VERSION}`).toEqual(latestTag);
+  });
+});
